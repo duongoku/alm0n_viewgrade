00171785dea3ea78e7b4def9fe96b4b4963fe015
duongoku <nguyenvubinhduong@gmail.com>
2022-06-26 22:42:49 +0700

    Refactoring

--- bot.py
+++ bot.py
@@ -17,27 +17,29 @@ class alm0n(commands.Bot):
 
 def run():
     try:
-        os.mkdir('temp')
+        os.mkdir(os.getenv("TEMPDIR"))
     except FileExistsError as error:
         print(error)
 
-    prefix = '~' if os.getenv('Path') else ';'
+    try:
+        os.mkdir(os.getenv("CACHEDIR"))
+    except FileExistsError as error:
+        print(error)
+
+    prefix = "~" if os.getenv("Path") else ";"
     intents = discord.Intents.all()
-    activity = discord.Game(name=f'{prefix}help')
+    activity = discord.Game(name=f"{prefix}help")
 
     bot = alm0n(
-        intents=intents,
-        command_prefix=prefix,
-        activity=activity,
-        help_command=None
+        intents=intents, command_prefix=prefix, activity=activity, help_command=None
     )
 
     bot.add_cog(Utility(bot))
 
-    bot.run(os.getenv('DISCORD_BOT_TOKEN'))
+    bot.run(os.getenv("DISCORD_BOT_TOKEN"))
 
-    print('Bot is down!')
+    print("Bot is down!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run()
